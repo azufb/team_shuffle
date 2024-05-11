93706730593db07521575221afe8af7ce6ede0ef
azufb <curiousazunas@gmail.com>
2024-05-11 15:03:09 +0900

    テーブルのヘッダー項目を定義

--- src/CONST.ts
+++ src/CONST.ts
@@ -1,1 +1,5 @@
 export const LOCAL_STORAGE_ALL_MEMBERS_KEY = "allMembers";
+
+export const MembersTableHeaders: string[] = ["ID", "名前", "シャッフル対象"];
+
+export const ShuffleResultTableHeaders: string[] = ["ID", "名前"];
