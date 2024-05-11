99fd066e605fe24d3ccb778ad70a8e7752e47548
azufb <curiousazunas@gmail.com>
2024-05-11 19:47:13 +0900

    それっぽい修正

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import "./index.css";
 import "tailwindcss/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 
+// service workerの登録
+import { registerSW } from "virtual:pwa-register";
+registerSW();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
