e8b85e4a7eb73d582df86153d4ecc88622a255fe
azufb <curiousazunas@gmail.com>
2024-05-11 17:23:31 +0900

    追記

--- src/components/membersList/MembersList.tsx
+++ src/components/membersList/MembersList.tsx
@@ -9,7 +9,8 @@ type PropsType = {
 export const MembersList = ({ allMembers }: PropsType) => {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-white font-bold">メンバーの登録状況</p>
+      <h2 className="text-white font-bold">メンバーの登録状況</h2>
+      <p className="text-white">以下のメンバーが登録されています。</p>
       <Table headers={SHUFFLE_RESULT_TABLE_HEADERS} items={allMembers} />
       <p className="text-white">合計：{allMembers.length}人</p>
     </div>
